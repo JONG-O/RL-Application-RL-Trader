6d8402585c45170afc2e8ddf147995578226e95b
JONG-O <fido72@gmail.com>
2021-03-17 16:59:48 +0900

    Delete market.py

--- market.py
+++
@@ -1,8 +0,0 @@
-
-class SmMarket:
-    def __init__(self):
-        self.name = ""
-        self.product_dic = {}
-
-    def add_category(self, product):
-        self.product_dic[product.code] = product
